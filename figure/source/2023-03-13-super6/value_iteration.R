66960034610325e109e87684c8b846f6ef926f84
hoehleatsu <hoehle@math.su.se>
2023-03-16 16:45:17 +0100

    added python version of the value iteration

--- figure/source/2023-03-13-super6/value_iteration.R
+++ figure/source/2023-03-13-super6/value_iteration.R
@@ -87,79 +87,79 @@ make_states <- function(n) {
   return(states)  
 }
 
-# Number of initial sticks left in the game 
-n <- 8
-states <- make_states(n)
-n_states <- length(states)
-## Storage of the value function
-U_vec <- U_vec_prime <- rep(0, n_states) #
-## Storage of the policy/strategy
-pi <- rep(NA, n_states)
-## Loop controls
-stop <- FALSE
-epsilon <- 1e-10
-iter <- 0
-
-# Value iteration 
-while (!stop) {
-  iter <- iter + 1
-  Delta <- 0
-  for (idx in seq_len(n_states)) {
-    # Translate to integer configuration
-    state <- state_str2num(states[idx])
-  
-    # The new states (might not be possible!)
-    state_six      <- state + c(  0, -1, 0, 0)       ; state_six[4] = 0 #next move is decidable
-    state_free     <- state + c(  1, -1, 0, 0)       ; state_free[4] = 0 #next move is decidable
-    state_occupied <- swap(state + c( -1,  1, 0, 0)) ; state_occupied[4] = 1 #forced to throw in round 1
-    state_swap     <- swap(state)                    ; state_swap[4] = 1 #forced to throw in round 1
-    
-    # Number of sticks in the lid
-    i <- state[1]
-    
-    #Q(s, continue)
-    qs_continue <- 
-        1/6               * (R(state_six)  +  U(state_six)) + 
-        (if (i<5) (5-i)/6 * (R(state_free) +  U(state_free)) else 0) + 
-        (if (i>0) i/6     * (R(state_occupied) - U(state_occupied)) else 0)
-    
-    #Q(s, stop)
-    qs_stop <- -(R(state_swap) + U(state_swap))
-  
-    # Updated value & strategy if there is a choice.
-    if (state[4] == 0) {
-      U_vec_prime[idx] <- max(qs_continue, qs_stop)
-      pi[idx] <- qs_continue > qs_stop
-    } else { #no choice
-      U_vec_prime[idx] <- qs_continue
-      pi[idx] <- 1
-    }
-    
-    # Discrepancy
-    #cat("iter = ", iter, " idx = ", idx, " Delta=", Delta, "\n")
-    Delta <- max(Delta, abs(U_vec[idx] - U_vec_prime[idx]))
-  }
-  #Delta <-  max(abs(U_vec - U_vec_prime))
-  #if (iter>=100) { browser() }
-  # Update the value function
-  U_vec <- U_vec_prime
-  # Continue the value iteration?
-  cat("iter = ", iter, ", Delta =", Delta, "\n")
-  stop <- Delta <= epsilon
-  
- 
-}
-
-# Make a tibble containing the strategy
-strategy <- tibble(state=states, value=U_vec, strategy=pi, prob=1/2*U_vec+1/2) %>% print(n=Inf)
-strategy <- strategy %>% 
-  tidyr::separate_wider_delim(state, delim="/", names=c("i","j","k", "forced_move")) %>% 
-  mutate(across(c(i,j,k, forced_move), as.numeric)) %>% 
-  arrange(desc(i), desc(j), desc(k))
-strategy
-
-strategy %>% print(n=Inf)
-strategy %>% filter(!forced_move) %>% print(n=Inf)
+# # Number of initial sticks left in the game 
+# n <- 8
+# states <- make_states(n)
+# n_states <- length(states)
+# ## Storage of the value function
+# U_vec <- U_vec_prime <- rep(0, n_states) #
+# ## Storage of the policy/strategy
+# pi <- rep(NA, n_states)
+# ## Loop controls
+# stop <- FALSE
+# epsilon <- 1e-10
+# iter <- 0
+# 
+# # Value iteration 
+# while (!stop) {
+#   iter <- iter + 1
+#   Delta <- 0
+#   for (idx in seq_len(n_states)) {
+#     # Translate to integer configuration
+#     state <- state_str2num(states[idx])
+#   
+#     # The new states (might not be possible!)
+#     state_six      <- state + c(  0, -1, 0, 0)       ; state_six[4] = 0 #next move is decidable
+#     state_free     <- state + c(  1, -1, 0, 0)       ; state_free[4] = 0 #next move is decidable
+#     state_occupied <- swap(state + c( -1,  1, 0, 0)) ; state_occupied[4] = 1 #forced to throw in round 1
+#     state_swap     <- swap(state)                    ; state_swap[4] = 1 #forced to throw in round 1
+#     
+#     # Number of sticks in the lid
+#     i <- state[1]
+#     
+#     #Q(s, continue)
+#     qs_continue <- 
+#         1/6               * (R(state_six)  +  U(state_six)) + 
+#         (if (i<5) (5-i)/6 * (R(state_free) +  U(state_free)) else 0) + 
+#         (if (i>0) i/6     * (R(state_occupied) - U(state_occupied)) else 0)
+#     
+#     #Q(s, stop)
+#     qs_stop <- -(R(state_swap) + U(state_swap))
+#   
+#     # Updated value & strategy if there is a choice.
+#     if (state[4] == 0) {
+#       U_vec_prime[idx] <- max(qs_continue, qs_stop)
+#       pi[idx] <- qs_continue > qs_stop
+#     } else { #no choice
+#       U_vec_prime[idx] <- qs_continue
+#       pi[idx] <- 1
+#     }
+#     
+#     # Discrepancy
+#     #cat("iter = ", iter, " idx = ", idx, " Delta=", Delta, "\n")
+#     Delta <- max(Delta, abs(U_vec[idx] - U_vec_prime[idx]))
+#   }
+#   #Delta <-  max(abs(U_vec - U_vec_prime))
+#   #if (iter>=100) { browser() }
+#   # Update the value function
+#   U_vec <- U_vec_prime
+#   # Continue the value iteration?
+#   cat("iter = ", iter, ", Delta =", Delta, "\n")
+#   stop <- Delta <= epsilon
+#   
+#  
+# }
+# 
+# # Make a tibble containing the strategy
+# strategy <- tibble(state=states, value=U_vec, strategy=pi, prob=1/2*U_vec+1/2) %>% print(n=Inf)
+# strategy <- strategy %>% 
+#   tidyr::separate_wider_delim(state, delim="/", names=c("i","j","k", "forced_move")) %>% 
+#   mutate(across(c(i,j,k, forced_move), as.numeric)) %>% 
+#   arrange(desc(i), desc(j), desc(k))
+# strategy
+# 
+# strategy %>% print(n=Inf)
+# strategy %>% filter(!forced_move) %>% print(n=Inf)
 
 ## Same as a function
 optimal_strategy <- function(n) {
@@ -253,8 +253,14 @@ optimal_strategy <- function(n) {
   return(strategy)
 }
 
+optimal_strategy(n=6)
+
+#########################################################
+# Compute probability of winning as start player
+#########################################################
+
 ## Opening round one throw of the dice each, initially m sticks
-m <- 2
+m <- 4
 
 # Function to find probability to win
 p_win <- function(state) {
